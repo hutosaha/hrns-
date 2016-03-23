5ae4de04359886ab510ccc678c5118f85a32594f
tormod17 <tormodsmith@gmail.com>
2016-03-23 18:15:50 +0000

    added test coverage and integration

--- test/test.js
+++ test/test.js
@@ -1,1 +1,36 @@
-const server = require('../start.js');
+'use strict'
+
+const server = require('../index.js');
+const test = require('tape');
+const Hapi = require('hapi');
+const shot = require('shot')
+
+ 
+test('Server is running', (t)=>{
+
+	server.init(0 , (err,server) => {
+		t.equal(server instanceof Hapi.Server, true ,' Server is an instance of the Hapi Server');
+		server.stop(t.end);
+	})
+});
+
+
+test('root endpoint serves home page ', (t)=>{
+	let actual, expected;
+	
+	var options = {
+		url:'/',
+		method:'GET'
+	};
+	server.init(0,(err,server) => {
+
+		server.inject(options, (response)=>{
+			console.log(response.statusCode);
+			actual =response.statusCode;
+			expected = 200;
+			t.equal(actual,expected, 'root takes us to home');
+		});
+		server.stop(t.end);
+	});
+
+});
