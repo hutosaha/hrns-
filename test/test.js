338c2cf8db0e65eef17628bafd12318567cb83ec
tormod17 <tormodsmith@gmail.com>
2016-03-24 17:01:45 +0000

    updating testing routes #1

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ const test = require('tape');
 const Hapi = require('hapi');
 const shot = require('shot');
 const redis = require('../lib/db/client.js');
+const fs = require('fs');
 
 test('Server is running', (t) => {
 
@@ -12,11 +13,11 @@ test('Server is running', (t) => {
 
 		t.equal(server instanceof Hapi.Server, true ,' Server is an instance of the Hapi Server');
 		server.stop(t.end);
-		redis.quit();
+	
 	})
 });
 
-test('root endpoint serves home page ', (t) => {
+test('root endpoint serves home page for new user or expired users', (t) => {
 	let actual, expected;
 
 	let options = {
@@ -27,11 +28,36 @@ test('root endpoint serves home page ', (t) => {
 	server.init(0,(err,server) => {
 
 		server.inject(options, (response) => {
-
+			
 			actual = response.statusCode;
 			expected = 200;
-			t.equal(actual,expected, 'root takes us to home');
-			server.stop(t.end);
+			t.equal(actual, expected, 'the / response status code 200');
+			
+			actual = fs.readFileSync('./public/views/home.html').toString();
+			expected = response.payload;
+			t.equal(actual,expected , 'new user gets home page');
+		
 		});
+
+		options.credentials = {
+    		scope:'client'
+    	};
+	
+		server.inject(options, (response) => {
+			console.log('cleint Dashboard', response.payload);
+			
+			actual = response.statusCode;
+        	expected = 200;
+        	t.equal(actual,expected,'scope client / with status code 200');
+			
+			actual = fs.readFileSync('./public/views/client.html').toString();
+			expected = response.payload;
+			t.equal(actual,expected , 'approved client gets client page');
+		
+		});
+
+		server.stop(t.end);
+		redis.quit();
 	});
 });
+// last test call redis.quit();
