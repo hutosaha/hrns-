be5a0a5f221646c6f9cb4bbae903036961e662c1
tormod17 <tormodsmith@gmail.com>
2016-04-11 10:50:22 +0100

    added endpoint tests and merged branches

--- test/test.js
+++ test/test.js
@@ -17,14 +17,16 @@ const candidateCookie = 'user=Fe26.2**860857e13a2898bc95038bc0490afb784896998c6b
 const noScopeCookie = 'user=Fe26.2**622733e4c21b742873bb056f797ce5609657139d8e2ad39469a882be274e9e3c*rKKeRkzDFpnQXVothS8ReA*r8JUw46OXLmFHlfIVWDwC2-P7fy0TJvAU9oGZbJ89-6hxA-OR1hMN7b0kLC8enT5-i1R39DPmm06vV309OjFrkuhqV7tW5zGTt7j5WyxeRv-iTXd0fEL5S-4G6DXA3DzD4opJetQF9qo42h6djabt-eG-7KCquJfWMf9w786OR8NnWRiR1lgxF2LHjYKBLTRUljpfReks2JYr6S1A0QrnH-4LXyCNZRrQ9jwwwIRozuopSz6lgQXHQ32mTbqD7BnfT_LQe9tbaJoaB6_MlxYJRVKVQESU052MDNqJRcQOXU71PXRiVoPJ2f89ZfEbF1v0hpI2OMR7BVvI18I7amRQyb7WqNeR7RVIGvgF7SuoiaCDGcCxVcLzZnperCco85Vam3D89cYeJATa__oBkBb1mvcAB38SYVLd_SeS7ssz9KK_YEYTeUg276SVK1USJNk77j8fwfVKNqKCdkTvosRYp4ZVPgHzaG-3Ri5YSuXTIBmZm8Kdf_09SLb1P1jkEdlym3XHf3RFxCGrnnvzDFkHPtjyQpUiLgBI-_hwOnoQBTB9ejxoVaCYBdum6Vh6VIG7LGLCNQccaifRxVy7VplxG0YYTbKrJBKVeND3FbzhrXTHgG542syXVD-6KW-Nw-poYnCr3zIPRoY9XXbWOV54LTFnFnvvtFCl3BWgnIEQ5xWSPGModOU8UwnjHMLWASlahdswgiYiNpUyv1Rulw47jKMvYzc0jO8DK3uvGUVwXVIt3carC6k2Gz2OcAPvMZdBuxsrNfHIEra4p8kgM3cvbjCNZask6pmBADPkeB83BFapxs9Fh08OJ8vDNd5zjA7cb297na-6D5tFn1VpAtdL2fBrT9wppneFLx4IQ0bpvVihjm3otBzaur_kChqTOU2VQIZI8a-kV4hf-HxCLfqup5opzFIlPez39sZ_XetmuI**f24ab47844e37d61a340d975cd8df1b5b08e4aacf90c2ed4c8414ffa8b2d6495*J6qm-Ms3HWmHwu2XNHoE3yBhHEoYt_m5vJAQdO_truo';
 const notAdminCookie = 'user=Fe26.2**4b2de8ebe8b88339f18074afecda5a092355d16f15d5aa85809acef2b0f9dffa*hq0Wz1_Khdi8VZNcbt3xkA*RWl0rd6Ho5eA6SVZjhXmLUf3nMAnEiK3EcJhb882PEyesF6oVpIpx30Fnkg3FnpDw9xPcPZEZ26JQ4sY1uU7pKC4G9YqzhNIgNllY7UNdIKp5yKh3-hanAyV2HtIaDjK9Dc8cInXVEdskpOSCySKyuxj23S3mkh8E640nAIpIcQa5_LRpv9i7It-CM_CNnrrBXj6hnCPYEJJaoNDGF_pYIe_mkjdbAXPsKA_q9oIUzhzIHlj0ngwFYX1Oj1MZ8pvsRKSXzVdu_JofoNXTjDFxIyI5omX90NqRG83NxCmVqT8RouGetQrFo7oyth63dGmXtp2NK3XUGkccvR1cilZOqQC0CoRgcdNfSP4VCDO-5cqtDI29Wx0Cyro6rvAbUWBVwgMPzuZ2qke4V6g3WDzSfimHGTk9jY_Er7IyMj-8PiOhGE_VJHaIMhpB8HTGhKafoqBYO2dXXaSOs3ICm_qF5dohvDwqYOHstZ5mHH5FTjSIY1-yTXftplncXleJ5KwqFWVoivA2am3snSSXFiLsOKnYUdrglt2f6PncgCrybzyPG9M8xtquNZ9wC_IR_beIbgzm01xSf64yNkNk2RWqoN6pWV5IHwAMSWiXIoIje2GhZ0iBhzBaEOdExStv6qc_lTJSQ_65ycO7yInh5euhjoRBv7U70fpeI3rOdMfd7gLNCRWfqP_RtAEGbRcwXBueBGDKsAQ-KN3viRwVqkm-bZWjj4dLuTwyO4n4LSkmF2_JFK3viPY52JPldRbofwd_ocj3V7i2HeQkE4EYJa1qpYz2w6kWlnA4SpuUoyGUDzPPinU65xpblJk7MonnsNEaLJi3rYrp94KnlTfhLf2yhAe4jvDti4kBnLuHZDr5tw_JFPjE44YzueOISDwfRYlna1QaZ2TFSCICOgl2lvxXhYPpcn1xiCdz-pRMr-xaLY**47cf87ef8dcf760748a4d41aae8b882548c0293dbe30c2a648e398b32dfc8a44*IYtWcygT8kCxCtoHGZhXTwKEyvK7BMpp1X-8X8AoYxk';
 const agencyCookie = 'user=Fe26.2**2f9bfa92d78c3aea2756bb7244cd0fa697f5fe80041c83ddb83e6ac0d5ceb807*qalsdmnPb1L3CcqkGsBy9w*q7ySwPNBUUj_ZfQ93ZQioe-Wd825_7Io9usc1qqjLcjIXmBReXgtHYsgJlcwzmtgEGJnPQ8UCf5uTB8kX0zScgLUn5vDc_ts6pJp9pirKgDfGMfvDMpfEBLREa1vcqsoqtUIxcrfpqWVaZg6O8sxD7JIVK_2oiW_jNASu1yp38QHP3IqEluRG6XSUZjVHTGRPjfBP3azBb6LKY6bvnr4bDIGyfuV_FYHicUqH0C8z9rOTex5aFf5ci2zGmu6S6B15Ei0Td86antu-8h0g1aUvapLXoTmEW6mFMR4k1Lv8yPcu9mK6T7dz66xFbVn15csX7QtQ37FxcXcg2pVsZN8lv36xSIw9fNUeQegqMyW7ayoUdiQ-NG9ai-q1zd-09vyzEcAnjcotZf58ZQoRQA4xPh1D-ygCG8IdE-6JcNaPHWmL2GUUSr7FeIsy2JcqmJu8m0HTSYV8iP1lMmoBac6_ulaMDaWpF0WHBm3WThLrh-ddfP98KRJKLsPXEcOFsMr1ZsD7VgJmV-L5ptq7dHGXNPbfCls6FkugPJb7hEen_EJkL6fB5KtzuYeqvDHG2OT3pw_ZyseQbDk21u1AeyELViYDkxh5lGTwk4Rth3lGj84ulpMFzP7xA-JSjuzUtylXPcujgPNpTeFlBM-rB1Z7U_8GY5X-twQZ9AExH7u8CbYpb0DDZNq-OKTpTJoj8do5eOcB4jtqWMxvOrOjwrdCOLV1AGmPnuwAcR4LcbgSzZEm4gPyD0GNc3VK4jItktyZAJAYamyqB25uOtDKczaIMgJkce_TuIPHzSLvR3Lh3xG3xQZqi2uqe3bLeQ6g3b-HpdKd0ulMMMWm8dzzbify7TXyWxq7PlKxEV0lyGKu6e5j8-sB3KyVc9QeV95ylHp7wKkBy6EUAxUExLnWgarz5AMnyA5ysIVD0aiiLDr4SB2JuDOTQdNxMQeS8itqfUB**c1f925a797b4a0fa3c64a9b9e513687b58cf40e888ea74694f788eaaeb264393*Fo3IsFp8deYH297vYIIyPprFlbTFuQ-suuj6hBru_xw';
+//const approvedAgencyCookie ='user=Fe26.2**3db2e6c901be1f70149f459af1279ab5e302777081f5b52cd69189e28b8ad206*MWA6_INFGWSt4pP3VPWnLA*Hh35_4S0vg12pjn9wn-K-qNCEcyO_jRPW-1wihZbXoJ0BAvUUq_Ww7ZSDtK7ium2_HgU-BoJ536BJfHMES865-hUz-8cbcae4f3aef3f0255983b06277f6262d5bcb3c5a9d5ff966ea595684d599a72-Yrx8n8EYezautYx0XVLI6dGvGISJ6wZewxmp3RYKBnOQtQ1ptaqJwTyOwYJZ10Xs6SEmuMg8n6kMIwosw0jy6UnVOGVCyQFoIiGU9OuN_kxnMkbL6D7qj9xA9BX0vmicEQuSuRdP-TaN5iUbfTALkFnDaM4xQpaZsOUAYppM8uPambOTAhUXkJQasMyPbg30LNW1BYq005tpkZHa3qvzzLpcy4e5RN5PuRdXmCpi4LCY5XbuJ0u6Wp-Eqn1TLLAe8fLQwbFGvg4mdD33Dgrr3btX1BMGMzyme8bBYZHaJ6nj_1apTcFqWGZfcF_WB9MMoNud7_qDtAFeCSXG2biLHRBca25C6JhtM91RP9DL4g-lUwTiNSU9F_CaRbhZiUu7nNVBnloDrgbId0GuTk9bmYyiHlbuPQdyeZXGAnRFXErBW14cH**d270cd1db70e2b409dbf305cd91c7b71332b66d4636756ae5ef71d0cb8895163*EwfpjvrX1m37J5UYJzx5Ey_bk-HU-TqQ0V0lX3KnZEM';
 const nonExistingUserCookie = 'user=Fe26.2**e4145d6d76cc73ecb11c24227601aa2a0a0ce95391a315d935586962a256c683*v4b4bmmaO0zDkqoV5C3d-w*vockqifTNv0xt2Ie-Xba-2jcz3_0kOpffVaxZtUngfbUu0ij8dNA2A0RvHoEt-3qn2oZ-SSyLfpW4bL2_GgcPllzkBZaAxoMNDrTdp13-WbT8Rd7yhLm6FpdUpczmSss7EfxdS5KqDH1RP7cQIy7tHw9doDhfi5Rap3cQjt4hAuTAR1-kh2wDLul-uvno0tV5_RsgUzvHPMGzd0Yri4wvonsyosJDhvuV5JlOvSImG7DDTR-riHcQeZkURQlq9Vt75ylr7ue3ex8wYWZdrC2Z5jFm9md6BI0w_U5Orcbm4hZGngjdOBWP-1igGO7J_HcKpE16EprW_VzvDPpu978lJQFIuj1BWHFT49Klqlz-v1H5g9Ss1n5GEsJE81SbD3bp3hZakuzsj67nZg83uK9iqyCpycmWKxTemp_IcFYfiAMsZmy3t7loU60tSwurMIaFqu0PLeBZLrjmHxPiBt4bvbY2CVcBwm4UC7Nv8cx2TxgAMp10tYFAaLylXdZK54-wgg-wroVnXTA0VJbVeHaczDLYXUg4Z95SFi5YtMy8X5PvujI36fRwTmlQJkQsXE5GKfgzgpyISFBXET9HWqQFU-Q24VYvGQiZw9nuakJQdB1vA6Tjg6thjZ35wEZwqF0lt6hqvJAwy6QY5lemxvqLhf8qGTRb88i-3xAwN7-fcIE2n20DwsZMwVnk774K9QBgngvmKnAai6fqyvno0cNCLl88eozuIwOTinHJ4PpRjna2lpOBUUlH5asIfovSfcr6dFl1TIU96wqd0lJ8HYjOLgyQxVCLIFnNzLj3iGK6bjTAMWf4uQkcYdWAb0WeKTfcYKlA8eQJ388UGsKzVKAooizp8zRsdKD4ZdmN0G4xcX58bOJjzUNkI_4pjkq-i0b**0e0f3a0c5aef2fbced0fcbac5e33a65b895a6893b5174dd876e43edacef3377f*Mb1DlanKlAKYzA7ECEIsoUWTJRlwjbtaUXlaxjlK0p4';
 
 
 const agencySignupPayload = { contactName: 'Joe Bloggs', companyName: 'google', contactNumber: '0823748237', email: 'fac@hotmail.com', companySize: '50-200', agencySpecialism: 'Creative' };
 const incorrectAgencySignupPayload = { contactName: 2, companyName: 23, contactNumber: 'asdasd' };
 const clientSignupPayload = { contactName: 'Huw Davies', email: 'me@me.com', contactNumber: '08372974723', companyName: 'Facebook Ltd.', companyDescription: 'Social media application', companySize: '500+', website: 'http://facebook.com', twitter: '@facebook' };
-const jobPayload = {jobTitle: 'Tester', jobDescription: 'testing everything', jobCategory: 'test', teamCulture: 'anal', typesOfProjects: 'tests', teamSize: 5, skillOne: 'test', skillTwo: 'test again', skillThree: 'test more', personality: 'persistant', salary: 100000, searchProgress: 'slow', searchDeadline: '12\/12\/2016'};
-    
+const jobPayload = { jobTitle: 'Tester', jobDescription: 'testing everything', jobCategory: 'test', teamCulture: 'anal', typesOfProjects: 'tests', teamSize: 5, skillOne: 'test', skillTwo: 'test again', skillThree: 'test more', personality: 'persistant', salary: 100000, searchProgress: 'slow', searchDeadline: '12\/12\/2016' };
+const cvPayload = { candidateName: 'Johnny Rotten', jobTitle: 'muppet', email: 'test@test.com', contactNumber: '0823748237', salary: '30000', linkedInProfile: 'https://linkedin', file_name: 'testcv.doc', file_url: 'https://torhuw-hrns.s3.amazonaws.com/testcv.doc'};
+
 
 let options;
 
@@ -55,7 +57,7 @@ server.init(0, (err, server) => {
                 t.equal(res.statusCode, statusCode, message);
                 t.end();
             });
-            
+
         });
     };
 
@@ -94,7 +96,7 @@ server.init(0, (err, server) => {
             server.inject(options, (res) => {
                 let actual = res.payload.indexOf(expectedString) > -1;
                 let expected = true;
-                // console.log('PAYLOAD', res.payload);
+                //console.log('PAYLOAD', res.payload);
                 t.equal(actual, expected, message + ' ' + expectedString);
                 t.end();
             });
@@ -105,8 +107,6 @@ server.init(0, (err, server) => {
         t.equal(server instanceof Hapi.Server, true, ' Server is an instance of the Hapi Server');
         t.end();
     });
-
-
 
 
     testEndPoint('/', 'GET', 200, 'responds with response 200');
@@ -154,7 +154,7 @@ server.init(0, (err, server) => {
 
     //curent test 
 
-    testPayload('/client/job/randomvid', 'GET', 'Client\'s Job Page', 'payload contains view with:', clientCookie);
+    testPayload('/client/job/randomvid', 'GET', 'Sorry, something went wrong', 'payload contains view with:', clientCookie);
 
     client.hset('vid', 'jobTitle', 'tester', () => {
         testPayload('/client/job/vid', 'GET', 'Client\'s Job Page', 'payload contains view with:', clientCookie);
@@ -218,19 +218,39 @@ server.init(0, (err, server) => {
 
     testEndPoint('/submitjob', 'GET', 200, 'endpoint responds with:', clientCookie);
 
-    
-    // tests that depends on liveJobs being empty
-    
+
+    // tests that depends on liveJobs being empty and send multiple emails to agencies. 
+
     client.del('liveJobs', () => { // 2 callbacks required because a redis test was making 1 callback fail :(
         client.del('liveJobs', () => {
             testPayload('/adminvacancies', 'GET', 'No live vacancies', 'correct message delivered for no vacancies', adminCookie);
             testPayload('/agency', 'GET', 'There are no vacancies', 'agency homepage apologies with no vacancies', agencyCookie);
             testEndPoint('/submitjob', 'POST', 200, 'endpoint responds with:', clientCookie, jobPayload, 'testid');
+     
+          });
+     });
+
+    testEndPoint('/submitvacancycv/vid','POST', 200, 'endpoint responds with:', agencyCookie, cvPayload );
+
+    client.hset('id', 'type', 'agency', () => {  
+        client.sadd('approvedUsers', 'id', () => {
+        testEndPoint('/userinfo/client','GET', 200, 'endpoint responds with:', adminCookie );
+        testEndPoint('/userinfo/agencies','GET', 200, 'endpoint responds with:', adminCookie );
         });
     });
+  
+
+    client.del('approvedUsers', () => {
+        testEndPoint('/userinfo/client','GET', 200, 'endpoint responds with:', adminCookie );
+    });
+    
+ 
+  
+
 
  
+    //client.flushdb();
     server.stop();
 
- // has to be here to prevent other tests from hanging
+    // has to be here to prevent other tests from hanging
 });
