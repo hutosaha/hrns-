f23918260313d30d924412af0c6a9fbb889c3f85
tormod17 <tormodsmith@gmail.com>
2016-04-19 15:33:40 +0100

    added ajax adminjob #17

---
+++ public/js/app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var $ = window.$;
+
+$(function() {
+$('.ui.dropdown')
+    .dropdown('set selected', 'value');
+
+$('#submitRatings').on('click', function() {
+    var allInputs = $(":input[type=hidden]");
+
+    allInputs.each(function() {
+        var rating = $(this).val();
+        var cvid = $(this).attr("id");
+        var vid = $(this).attr("name");
+        var aEmail = $(this).attr("data-aEmail");
+        console.log('allInputs', rating, vid, cvid, aEmail);
+        
+        if(rating){
+              $.ajax({
+                  url: '/rating/',
+                  data: {
+                      cvid: cvid,
+                      vid: vid,
+                      rating: rating,
+                      email: aEmail
+                  },
+                  async:true,
+                  success: function(data) {
+                    $('#candidates').html('<h3>'+data+'<h3>');
+                  },
+                  error:function(thrownError){
+                       console.log(thrownError);
+                  }
+
+
+          });
+        }
+    });
+
+  });
+ 
+});
+
+
+
+
