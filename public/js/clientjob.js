862d49c3ef18de9d1546e593a67cdeb61c278303
Huw <hdrdavies@gmail.com>
2016-04-20 15:13:12 +0100

    configuring for heroku and tidying up console.logs

--- public/js/clientjob.js
+++ public/js/clientjob.js
@@ -16,31 +16,20 @@ $(document).ready(function() {
     vid = $(this).data('vid');
     agencyEmail = $(this).data('agency-email');
 
-    console.log('###candidateName', candidateName);
-    console.log('###cvid', cvid);
-    console.log('###vid', vid);
-    console.log('###agencyEmail', agencyEmail);
-
     if (candidateName) {
       $('#modal-heading').html('Sorry to hear you want to reject ' + candidateName);
     }
 
     $('.ui.modal.reject-modal').modal('show');
     $('.ui.radio.checkbox').checkbox();
-    
+
   });
 
   $('#modal-submit').click(function() {
-    console.log('candidateName', candidateName);
-    console.log('cvid', cvid);
-    console.log('vid', vid);
-    console.log('agencyEmail', agencyEmail);
 
     var reason = $('input[name="rejection-reason"]:checked').val();
 
-    console.log('reason', reason);
-
-    // $('#modal-submit').toggleClass('loading');
+    $('#modal-submit').toggleClass('loading');
 
     $.ajax({
         url: '/client/job/reject',
@@ -53,16 +42,13 @@ $(document).ready(function() {
         },
         async: true,
         success: function(res) {
-          console.log('FE res', res);
           $('#modal-submit').remove();
           if (res) {
             $('#modal-message').toggleClass('hidden');
             $('#modal-message').html('Candidate successfully rejected!!');
-            // setTimeout(() => location = '/client/job/' + vid, 3000);
           } else {
             $('#modal-message').toggleClass('hidden');
             $('#modal-message').html('Something went wrong, please try again...');
-            // setTimeout(() => location = '/client/job/' + vid, 3000);
           }
         }
     });
