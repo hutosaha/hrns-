285418032dbe62aa28564f85838c74a46842b1ab
tormod17 <tormodsmith@gmail.com>
2016-04-22 13:00:06 +0100

    admin upload client logo

---
+++ public/js/approveusers.js
@@ -0,0 +1,31 @@
+var $ = window.$;
+
+(function() {
+
+$('.viewMore').on('click', function(){
+	$(this).parent().parent().parent().find('form').fadeToggle();
+  });
+
+$(':input[type=submit]').on('click',function(){
+	event.preventDefault();
+	var logo_url= $(this.form).find(':input[type=hidden]').val();
+	var id = $(this.form).attr('id');
+	var message =$(this.form).find('.message');
+		$.ajax({
+        url: '/admin/clientlogo/',
+        data: {
+          logo_url: logo_url,
+          id: id
+        },
+        async: true,
+        success: function(res) {
+           message.hide().html(res).fadeIn('slow');       
+        }
+    });
+
+
+});
+
+
+
+})();
