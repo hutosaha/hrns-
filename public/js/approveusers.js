942538b17b3c6e7c703a0ba262a42fe1a593654a
Huw <hdrdavies@gmail.com>
2016-04-26 10:52:11 +0100

    tidy up approveusers before changing hidden elements #97

--- public/js/approveusers.js
+++ public/js/approveusers.js
@@ -2,16 +2,17 @@ var $ = window.$;
 
 (function() {
 
-$('.viewMore').on('click', function(){
-	$(this).parent().parent().parent().find('form').fadeToggle();
+	$('.viewMore').on('click', function(){
+		$(this).parent().parent().parent().find('form').fadeToggle();
   });
 
-$(':input[type=submit]').on('click',function(){
-	event.preventDefault();
-	var logo_url= $(this.form).find(':input[type=hidden]').val();
-	var id = $(this.form).attr('id');
-	var message =$(this.form).find('.message');
-		$.ajax({
+	$(':input[type=submit]').on('click',function(){
+
+		event.preventDefault();
+		var logo_url= $(this.form).find(':input[type=hidden]').val();
+		var id = $(this.form).attr('id');
+		var message = $(this.form).find('.message');
+			$.ajax({
         url: '/approveusers/clientlogo/',
         data: {
           logo_url: logo_url,
@@ -19,13 +20,8 @@ $(':input[type=submit]').on('click',function(){
         },
         async: true,
         success: function(res) {
-           message.hide().html(res).fadeIn('slow');       
+           message.hide().html(res).fadeIn('slow');
         }
     });
-
-
-});
-
-
-
+	});
 })();
