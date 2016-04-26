0a1bd74cef62b348fbfdf8fc247caef1936046fc
Huw <hdrdavies@gmail.com>
2016-04-26 12:04:52 +0100

    view more functionality on userinfo #97

--- public/js/userinfo.js
+++ public/js/userinfo.js
@@ -3,3 +3,8 @@
 var $ = window.$;
 
  $('.menu .item').tab();
+
+ $('.viewMore').on('click', function(){
+   var id = $(this).data('id');
+   $(this).parent().parent().parent().find('#' + id).fadeToggle();
+ });
