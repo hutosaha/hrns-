262a507fda432751b1a495b6ee7799b519c38a50
tormod17 <tormodsmith@gmail.com>
2016-05-10 15:32:41 +0100

    file viewer pdf only #141

--- public/js/clientjob.js
+++ public/js/clientjob.js
@@ -7,83 +7,92 @@ $(document).ready(function() {
     $('.cv-viewer').click(function() {
         /// need to write file before iframe injection 
 
-
-        var candidateName = $(this).data('candidate-name');
+       var candidateName = $(this).data('candidate-name');
         var cvid = $(this).data('cvid');
         var vid = $(this).data('vid');
         var agencyEmail = $(this).data('agency-email');
         var agencyId = $(this).data('agency-id');
         var jobTitle = $(this).data('job-title');
 
-        $.ajax({
-            url: '/client/view-cv',
-            data: {
-                cvid: cvid,
-            },
-            async: true,
-            success: function(pathName) {
-                if (pathName) {
-                    // insert iframe with given path. 
-                    var src = "/public/assets/ViewerJS/#../Downloads/" + pathName;
-                    $('iframe').attr('src', src);
-                    }
-
-
-                }
-            }
-        });
-
-
-       
-        // make an ajax call to convert and write file . 
-        $('.ui.basic.doc-viewer.modal').modal('show');
-
-
-        $('.reject').click(function() {
-
-            $('.first.modal.reject-modal')
-                .modal('show');
-
-
-            var $inputs = $('input[name=rejection-reason]');
-            $('input[name=rejection-reason]').change(function() {
-                if (this.checked)
-                    $inputs.not(this).prop('checked', !this.checked);
-            });
-
-
-            $('.confirm-rejection-button').click(function() {
-                $('.first.modal.reject').modal('hide');
-                $('.second.modal.reject-modal').modal('show', '.first.modal .button');
-                $('.second.coupled.modal.accept-modal').modal('hide');
-
-
-                var reason = $('input[name="rejection-reason"]:checked').val();
-
+        const ext = cvid.substr(cvid.lastIndexOf('.')+1)
+        
+        if ( ext === 'pdf') {
+                $('iframe').attr('src', '');
                 $.ajax({
-                    url: '/client/job/reject',
+                    url: '/client/view-cv',
                     data: {
-                        candidateName: candidateName,
                         cvid: cvid,
-                        vid: vid,
-                        email: agencyEmail,
-                        reason: reason
                     },
                     async: true,
-                    success: function(res) {
-                        if (res) {
-                            var element = document.getElementById(cvid);
-                            element.remove();
-                            $('.ui.basic.doc-viewer.modal').modal('hide');
+                    success: function(pathName) {
+                            if (pathName) {
 
-                            if ($('.listView').length === 0) {
-                                $('.message').html('There are no candidates yet!');
-                            }
-                        }
+                                var src = "/public/assets/ViewerJS/#../Downloads/" +pathName;
+                                $('iframe').attr('src', src);
+                                setTimeout(function(){
+                                   document.getElementById('iframe').contentWindow.location.reload();
+                                },1500);
+
+                            };
                     }
                 });
-            });
-        });
+
+
+               
+                // make an ajax call to convert and write file . 
+                $('.ui.basic.doc-viewer.modal').modal('show');
+
+
+                $('.reject').click(function() {
+
+                    $('.first.modal.reject-modal')
+                        .modal('show');
+
+
+                    var $inputs = $('input[name=rejection-reason]');
+                    $('input[name=rejection-reason]').change(function() {
+                        if (this.checked)
+                            $inputs.not(this).prop('checked', !this.checked);
+                    });
+
+
+                    $('.confirm-rejection-button').click(function() {
+                        $('.first.modal.reject').modal('hide');
+                        $('.second.modal.reject-modal').modal('show', '.first.modal .button');
+                        $('.second.coupled.modal.accept-modal').modal('hide');
+
+
+                        var reason = $('input[name="rejection-reason"]:checked').val();
+
+                        $.ajax({
+                            url: '/client/job/reject',
+                            data: {
+                                candidateName: candidateName,
+                                cvid: cvid,
+                                vid: vid,
+                                email: agencyEmail,
+                                reason: reason
+                            },
+                            async: true,
+                            success: function(res) {
+                                if (res) {
+                                    var element = document.getElementById(cvid);
+                                    element.remove();
+                                    $('.ui.basic.doc-viewer.modal').modal('hide');
+
+                                    if ($('.listView').length === 0) {
+                                        $('.message').html('There are no candidates yet!');
+                                    }
+                                }
+                            }
+                        });
+                    });
+                });
+            } else {
+                $(this).html('Only pdf files');
+                $(this).attr('disabled','disabled');            
+        }
+    
 
 
         $('.accept').click(function() {
