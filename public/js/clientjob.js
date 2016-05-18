6929e9705e598320d8de06bde80c69517e7d5a4c
Marie Kasai <marie.kasai@live.co.uk>
2016-05-18 04:47:27 +0100

    re-places the rest of the original file in - no time to refactor from lines 103 to 279

--- public/js/clientjob.js
+++ public/js/clientjob.js
@@ -14,13 +14,9 @@ window.onbeforeunload = () => {
 
 $(document).ready(function() {
 
-
-    if ($('.listView').length === 0) {
-        $('.message').html('There are no candidates yet!');
-    }
+    if ($('.listView').length === 0) { $('.message').html('There are no candidates yet!'); }
 
     $('.cv-viewer').click(function() {
-        /// need to write file before iframe injection
 
         const relatedInfoObject = {
           candidateName: $(this).data('candidate-name'),
@@ -34,9 +30,6 @@ $(document).ready(function() {
         const msDocumentTypes = ['doc', 'docx'];
 
         const ext = cvUrl.substr(cvUrl.lastIndexOf('.')+1);
-        // FLOW: checkIfFileNameAlreadyDownloaded
-        // If downloaded - open in iframe
-        // If not downloaded - downloadFile
 
         isFileAlreadyDownloaded(cvUrl, (fileName) => {
           console.log(fileName);
@@ -65,7 +58,6 @@ $(document).ready(function() {
 });
 
 var isFileAlreadyDownloaded = (cvUrl, callback) => {
-//AJAX request to do fs.readdirSync --> check if file already exists
   $.ajax({
       url: '/client/file-exists',
       data: {
@@ -114,4 +106,182 @@ var viewFile = (fileName, relatedInfoObject) => {
   console.log(src);
   $('iframe').attr('src', src);
   $('.ui.basic.doc-viewer.modal').modal('show');
+
+  const candidateName = relatedInfoObject.candidateName;
+  const cvid = relatedInfoObject.cvid;
+  const vid = relatedInfoObject.vid;
+  const agencyEmail = relatedInfoObject.agencyEmail;
+  const agencyId = relatedInfoObject.agencyId;
+  const jobTitle = relatedInfoObject.jobTitle;
+
+  $('.reject').click(function() {
+
+     $('.first.modal.reject-modal')
+         .modal('show');
+
+     var $inputs = $('input[name=rejection-reason]');
+     $('input[name=rejection-reason]').change(function() {
+         if (this.checked)
+             $inputs.not(this).prop('checked', !this.checked);
+     });
+
+     $('.confirm-rejection-button').click(function() {
+         $('.first.modal.reject').modal('hide');
+         $('.second.modal.reject-modal').modal('show', '.first.modal .button');
+         $('.second.coupled.modal.accept-modal').modal('hide');
+
+         var reason = $('input[name="rejection-reason"]:checked').val();
+
+         $.ajax({
+             url: '/client/job/reject',
+             data: {
+                 candidateName: candidateName,
+                 cvid: cvid,
+                 vid: vid,
+                 email: agencyEmail,
+                 reason: reason
+             },
+             async: true,
+             success: function(res) {
+                 if (res) {
+                     var element = document.getElementById(cvid);
+                     element.remove();
+                     $('.ui.basic.doc-viewer.modal').modal('hide');
+
+                     if ($('.listView').length === 0) {
+                         $('.message').html('There are no candidates yet!');
+                     }
+                 }
+             }
+         });
+
+     });
+ });
+
+ $('.accept').click(function() {
+     $('.ui.basic.doc-viewer.modal').modal('hide');
+
+     $.ajax({
+         url: '/client/job/accept',
+         data: {
+             candidateName: candidateName,
+             cvid: cvid,
+             vid: vid,
+             email: agencyEmail,
+             agencyId: agencyId,
+             jobTitle: jobTitle
+         },
+         async: true,
+         success: function(res) {
+             if (res) {
+                 $('.doc-viewer.modal').hide();
+                 var element = document.getElementById(cvid);
+                 element.remove();
+                 if ($('.listView').length === 0) {
+                     $('.message').html('There are no new candidates for this position.');
+                 }
+             }
+         }
+     });
+ });
+
+ $('.reject-button').click(function() {
+
+     if (candidateName) { $('#modal-heading').html('Sorry to hear you want to reject ' + candidateName); }
+
+     $('.ui.checkbox').checkbox();
+
+     $('.coupled.modal.reject-modal')
+         .modal({
+             allowMultiple: false
+         });
+
+     $('.first.modal.reject-modal')
+         .modal('show');
+
+     var $inputs = $('input[name=rejection-reason]');
+     $('input[name=rejection-reason]').change(function() {
+         if (this.checked)
+             $inputs.not(this).prop('checked', !this.checked);
+     });
+
+     $('.confirm-rejection-button').click(function() {
+         $('.first.modal.reject').modal('hide');
+         $('.second.modal.reject-modal').modal('show', '.first.modal .button');
+         $('.second.coupled.modal.accept-modal').modal('hide');
+
+         var reason = $('input[name="rejection-reason"]:checked').val();
+
+         $.ajax({
+             url: '/client/job/reject',
+             data: {
+                 candidateName: candidateName,
+                 cvid: cvid,
+                 vid: vid,
+                 email: agencyEmail,
+                 reason: reason
+             },
+             async: true,
+             success: function(res) {
+                 if (res) {
+                     var element = document.getElementById(cvid);
+                     element.remove();
+                     if ($('.listView').length === 0) {
+                         $('.message').html('There are no candidates yet!');
+                     }
+                 }
+             }
+         });
+     });
+ });
+
+ $('.accept-button').click(function() {
+     var candidateName = $(this).data('candidate-name');
+     var cvid = $(this).data('cvid');
+     var vid = $(this).data('vid');
+     var agencyEmail = $(this).data('agency-email');
+     var agencyId = $(this).data('agency-id');
+     var jobTitle = $(this).data('job-title');
+     $('.accept-candidateName').html(candidateName);
+
+     $('.coupled.modal.accept-modal')
+         .modal({
+             allowMultiple: false
+         });
+     // attach events to buttons
+     $('.second.modal.accept-modal')
+         .modal('attach events', '.first.modal .button');
+     // show first now
+     $('.first.modal.accept-modal')
+         .modal('show');
+
+     $('.modal-cancel-acceptance').click(function() {
+         $('.first.modal.accept-modal').hide();
+     });
+
+     $('.modal-submit-acceptance-button').click(function() {
+
+         $.ajax({
+             url: '/client/job/accept',
+             data: {
+                 candidateName: candidateName,
+                 cvid: cvid,
+                 vid: vid,
+                 email: agencyEmail,
+                 agencyId: agencyId,
+                 jobTitle: jobTitle
+             },
+             async: true,
+             success: function(res) {
+                 if (res) {
+                     var element = document.getElementById(cvid);
+                     element.remove();
+                     if ($('.listView').length === 0) {
+                         $('.message').html('There are no new candidates for this position.');
+                     }
+                 }
+             }
+         });
+     });
+ });
 }
