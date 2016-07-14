e3af38462fa95d934044fb7559f57d2c97c78575
tormod17 <tormodsmith@gmail.com>
2016-07-14 10:43:46 +0100

    client side handlebars rendering

---
+++ public/js/harnesstalent.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var $ = window.$;
+
+(function() {
+
+    var CANDIDATES = {
+        init: function() {
+            this.fetch();
+            this.search();
+        },
+        fetch: function(query) {
+            var self = this;
+            $.ajax({
+                url: '/harnesstalent/results',
+                data: query,
+                success: function(arrayOfCandidates) {
+                    console.log('RESULTS FORM BE', arrayOfCandidates);
+                    var source = $('#candidates-template').html()
+                    var template = Handlebars.compile(source);
+                    var context = arrayOfCandidates;
+                    var populatedHTML = template(context);
+                    $('#candidates-container').html(populatedHTML)
+                }
+            })
+        },
+        search: function() {
+            $('.search').on('click', function() {
+
+                var queryObj = {
+                    jobTitle: $('select[name=jobTitle]').val(),
+                    company: $('select[name=company]').val(),
+                    jobCategory: $('select[name=jobCategory]').val(),
+                    city: $('select[name=city]').val(),
+                    salary: $('select[name=salary]').val(),
+                    contractType: $('select[name=contractType]').val()
+                }
+                var query = $.param(queryObj);
+                CANDIDATES.fetch(query)
+            })
+        }
+    }
+    CANDIDATES.init();
+})();
