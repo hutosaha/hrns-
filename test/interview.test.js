a8f59645eb2b84278670c08f35f461510af8394c
tormod17 <tormodsmith@gmail.com>
2016-07-26 14:41:19 +0100

    adding client email to harnesstalent interview.js

--- test/interview.test.js
+++ test/interview.test.js
@@ -115,7 +115,7 @@ server.init(0, (err, server) => {
                 client.hmsetAsync('testHTinterviewId' , interviewPayloadHT) ;
             })
             .then(() => {
-                //testEndPoint(server, '/interview/proposed', 'POST', 200, 'serves 200', clientCookie, newTimes);
+                testEndPoint(server, '/interview/proposed', 'POST', 200, 'serves 200', clientCookie, newTimes);
               //  testEndPoint(server, '/interview/email/test2interviewId', 'GET', 200, 'authed GET responds with 200', clientCookie);
               //  testEndPoint(server, '/interview/email/dummyinterviewId', 'GET', 200, 'authed GET responds with 200', clientCookie);
                // testEndPoint(server, '/change/interview', 'POST', 200, 'authed GET responds with 200', clientCookie, interviewPayload);
