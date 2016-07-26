52c04e9b3a2f47912e7d967d82da76f9bbbda926
tormod17 <tormodsmith@gmail.com>
2016-07-26 16:13:50 +0100

    doc viewer for job desc

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
-/*require('./utils.test.js');
+require('./utils.test.js');
 require('./otherplugins.test.js');
 require('./agencyplugins.test.js');
 require('./candidateplugins.test.js');
 require('./clientplugins.test.js');
 
 require('./harensstalent.test.js');
-require('./adminplugins.test.js');*/
+require('./adminplugins.test.js');
 require('./interview.test.js');
 require('./redis.test.js');
