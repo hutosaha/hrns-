0151eba9efa11586c41707c45decf54651fab18f
Jack <jctierney251@gmail.com>
2016-07-27 16:38:07 +0100

    remove function in cron timer js

--- crontimer.js
+++ crontimer.js
@@ -1,30 +1,49 @@
 const getSetMembersInfo = require('./lib/db/redis.js').getSetMembersInfo;
 const moment = require('moment');
 const removecv = require('./lib/db/redis.js').removecv;
-const emailAdminHarnessTalentCv = require('./lib/plugins/utils/mailgun.js').emailAdminHarnessTalentCv;
+const emailAgencyHarnessTalentExpiration = require('./lib/plugins/utils/mailgun.js').emailAgencyHarnessTalentExpiration;
 const mailgun = require('./lib/plugins/utils/mailgun.js');
+const client = require('./lib/db/client.js');
 
 console.log('I am in the Cron!!!');
 
 getSetMembersInfo('HarnessTalent', (array) => {
 
         array.forEach(function(arrayItem) {
-            const cvid = arrayItem.civd;
+          console.log(arrayItem);
+            const cvid = arrayItem.cvid;
+            console.log('ffffffffffffff',cvid);
             const agentsEmail = arrayItem.agencyEmail;
             const dateSubmitted = arrayItem.dateSubmitted;
             const expirationDate = moment(dateSubmitted).add(35, 'days').calendar();
             const deletionDate = moment(dateSubmitted).add(42, 'days').calendar();
+            console.log(deletionDate);
             const todaysDate = moment().calendar();
             switch (true) {
+                case (todaysDate === expirationDate):
+                var context = Object.assign({}, emailAgencyHarnessTalentExpiration);
+                context.html = context.html
+                .replace('-candidateName-', arrayItem.candidateName)
+                .replace('-agencyName-', arrayItem.agencyName);
+                context.subject = context.subject
+                .replace('-candidateName-', arrayItem.candidateName);
+                    context.to = agentsEmail;
+                     //REPLACE WITH AGENTSEMAIL!
+               return mailgun.messages().send(context, (err, body) => {
+                      if(err){
+                        console.log(err);
+                      } else {
+                        console.log(body);
+                      }
+                });
                 case (todaysDate === todaysDate):
-                console.log('email sent');
-                    emailAdminHarnessTalentCv.to = 'tormodsmith@gmail.com'; //REPLACE WITH AGENTSEMAIL!
-                    return mailgun.messages().send(emailAdminHarnessTalentCv);
-                case (todaysDate === deletionDate):
-                    console.log('Candidate removed from HarnessTalent');
-                    return removecv(cvid, 'HarnessTalent'); // delete from harness talent list
+                console.log('Candidate removed from HarnessTalent');
+                return client.srem('HarnessTalent', cvid, (err, reply)=>{
+                  if(err){console.log(err);
+                } else {console.log(reply);
+              }
+                }); // delete from harness talent list
             }
         });
     }
 );
-
