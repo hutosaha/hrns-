2166e8b6da3db1c0d14b8e3c82b29a01d7aef8ca
Jack <jctierney251@gmail.com>
2016-07-28 17:00:45 +0100

    mailto recruiter

--- public/js/sendcontactusform.js
+++ public/js/sendcontactusform.js
@@ -1,21 +1,21 @@
 'use strict'
-var  $ = window.$;
+var $ = window.$;
 
 
- function sendContactForm(){
+function sendContactForm() {
 
-  $('.submit.button').on('click', function(){
-  var firstName = $('input[name="firstName"]').val()
-  var lastName = $('input[name="lastName"]').val()
-  var email = $('input[name="email"]').val()
-  var phoneNumber = $('input[name="phoneNumber"]').val()
-  var text = $('textarea[name="text"]').val()
-  var mailto = 'tormodsmith@gmail.com'
+    $('.submit.button').on('click', function() {
+        var firstName = $('input[name="firstName"]').val()
+        var lastName = $('input[name="lastName"]').val()
+        var email = $('input[name="email"]').val()
+        var phoneNumber = $('input[name="phoneNumber"]').val()
+        var text = $('textarea[name="text"]').val()
+        var mailto = 'tormodsmith@gmail.com'
 
-  var body = encodeURIComponent(' Hi Harness, \n\n ' + text + '\n\n' + 'Kind regards, \n\n ' + firstName + lastName + '\n\n' + email + ' ' + phoneNumber );
-  window.open('mailto:' + mailto + '?subject=harness%20contact%20form&body=' + body );
-  window.close
-});
+        var body = encodeURIComponent(' Hi Harness, \n\n ' + text + '\n\n' + 'Kind regards, \n\n ' + firstName + lastName + '\n\n' + email + ' ' + phoneNumber);
+        window.location.href = 'mailto:' + mailto + '?subject=harness%20contact%20form&body=' + body
+
+    });
 
 }
 sendContactForm()
