e707ad7b49b2bcf64091c1f71f5eb75a2ce51bf5
tormod17 <tormodsmith@gmail.com>
2016-08-07 22:13:37 +0100

    candidate.js adding promise

--- test/candidateplugins.test.js
+++ test/candidateplugins.test.js
@@ -1,18 +1,28 @@
-const server           = require('../lib/index.js');
+const server = require('../lib/index.js');
 
-const testEndPoint     = require('./utils/utils.js').testEndPoint;
-const candidateCookie  = require('./utils/utils.js').candidateCookie;
+const testEndPoint = require('./utils/utils.js').testEndPoint;
+const candidateCookie = require('./utils/utils.js').candidateCookie;
 
-const candidatePayload = { candidateName: 'Johnny Rotten', jobTitle: 'muppet', email: 'test@test.com', contactNumber: '0823748237', salary: '30000', linkedInProfile: 'https://linkedin', file_name: 'testcv.doc', file_url: 'https://torhuw-hrns.s3.amazonaws.com/testcv.doc'};
+const candidatePayload = {
+    candidateName: 'Johnny Rotten',
+    jobTitle: 'muppet',
+    company:'Test LTD',
+    jobCategory:'UX',
+    email: 'tormodsmith@gmail.com',
+    contactNumber: '0823748237',
+    salary:'£30000',
+	contractType :'permanent' ,
+	location:'Glasgow',
+    linkedInProfile: 'https://linkedin',
+    file_name: 'testcv.doc',
+    file_url: 'https://torhuw-hrns.s3.amazonaws.com/testcv.doc'
+};
 
 
 server.init(0, (err, server) => {
 
-    testEndPoint(server, '/candidate', 'GET', 200, 'auth user responds with 200', candidateCookie);
-    testEndPoint(server, '/candidate', 'GET', 302, 'unauth user redirected to login');
-
-   // testEndPoint(server, '/candidate', 'POST', 200, 'unauth user redirected to login',candidateCookie, candidatePayload);
-
+   // testEndPoint(server, '/candidate', 'GET', 200, 'auth user responds with 200', candidateCookie);
+   //testEndPoint(server, '/candidate', 'GET', 302, 'unauth user redirected to login');
+    testEndPoint(server, '/candidate', 'POST', 200, 'unauth user redirected to login', candidateCookie, candidatePayload);
     server.stop();
-
 });
