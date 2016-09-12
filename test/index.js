a82119f1dac6df5aced494bcdbae64a757731567
tormod17 <tormodsmith@gmail.com>
2016-09-12 22:22:24 +0100

    Email agency on client shortlist rejection

--- test/index.js
+++ test/index.js
@@ -1,12 +1,13 @@
 /*require('./utils.test.js');
 require('./otherplugins.test.js');
 
-require('./clientplugins.test.js');
-require('./adminplugins.test.js');
+
 require('./redis.test.js');
 */
 //require('./interview.test.js');
 //require('./agencyplugins.test.js');
 //require('./harensstalent.test.js');
-require('./candidateplugins.test.js');
+//require('./candidateplugins.test.js');
+require('./clientplugins.test.js');
+require('./adminplugins.test.js');
 require('./redis.test.js');
