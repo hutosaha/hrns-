d94262d32a1b4c7a9e998ea0d40eb90bd60183a5
tormod17 <tormodsmith@gmail.com>
2016-09-12 16:44:02 +0100

    eamil accepted harness talent

--- test/adminplugins.test.js
+++ test/adminplugins.test.js
@@ -23,11 +23,9 @@ server.init(0, (err, server) => {
 
 
         testEndPoint(server, '/admindashboard', 'GET', 200, 'auth user responds with response 200', clientCookie);
-
         testEndPoint(server, '/admindashboard', 'GET', 200, 'nonAdmin responds with 200', notAdminCookie);
         testPayload(server, '/admindashboard', 'GET', 'it doesn\' look like you\'re an admin!', 'correct message displayed to non-Admin', notAdminCookie);
         testEndPoint(server, '/admindashboard', 'GET', 302, 'unauth user responds redirected to login');
-
         testEndPoint(server, '/adminvacancies', 'GET', 200, 'admin viewing vacancies responds with 200', adminCookie);
 
         client.hmsetAsync(vid, jobPayload)
@@ -46,16 +44,9 @@ server.init(0, (err, server) => {
             .catch();
 
         testEndPoint(server, '/admin/job/123', 'GET', 403, 'nonAdmin gets 403 response', notAdminCookie);
-
-
-        testEndPoint(server, '/approveuser/approve/VKADAKSD/test@test.com', 'GET', 200, 'server responds with new view', adminCookie);
-
-
-
+        testEndPoint(server, '/approveuser/approve/VKADAKSD/tormodsmith@gmail.com', 'GET', 200, 'server responds with new view', adminCookie);
         testEndPoint(server, '/approveuser/approve/id/me@me.com', 'GET', 302, 'unauthed  redirected to login');
-
         testEndPoint(server, '/approveusers', 'GET', 200, 'authed GET responds with 200', adminCookie);
-
 
         client.hset('id', 'type', 'agency', () => {
             client.sadd('approvedUsers', 'id', () => {
--- test/index.js
+++ test/index.js
@@ -3,9 +3,8 @@ require('./otherplugins.test.js');
 require('./candidateplugins.test.js');
 require('./clientplugins.test.js');
 require('./adminplugins.test.js');
+require('./redis.test.js');
+require('./interview.test.js');
+require('./agencyplugins.test.js');
 require('./harensstalent.test.js');
 require('./redis.test.js');
-//require('./interview.test.js');
-//require('./agencyplugins.test.js');
-require('./redis.test.js');
-
