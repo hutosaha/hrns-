faceea0b567bb4aad2a3f027f3b52fd9a9001f63
tormod17 <tormodsmith@gmail.com>
2016-09-21 23:26:46 +0100

    admin email tests

--- test/adminplugins.test.js
+++ test/adminplugins.test.js
@@ -15,6 +15,7 @@ const cvPayload =  require('./utils/utils.js').cvPayload;
 const ratingPayload =  require('./utils/utils.js').ratingPayload;
 
 const jobPayload =require('./utils/utils.js').jobPayload;
+const clientPayload = require('./utils/utils.js').clientPayload;
 
 const vid = 'test3Vid';
 const cvid = 'testCvid'
@@ -27,13 +28,15 @@ server.init(0, (err, server) => {
                 client.sadd(vid + 'adminShortlist', vid)
                 client.hmset(cvid, cvPayload);
                 client.hmset(vid, jobPayload);
+                client.hmset('iIkUSpzijO', clientPayload);
                 client.hset('id', 'type', 'agency');
                 client.sadd('approvedUsers', 'id');
+                client.sadd('awaitingApproval','iIkUSpzijO');
             })
             .then(() => {
                 testEndPoint(server, '/admindashboard', 'GET', 200, 'auth user responds with response 200', clientCookie);
                 testEndPoint(server, '/admindashboard', 'GET', 200, 'nonAdmin responds with 200', notAdminCookie);
-                testPayload(server, '/admindashboard', 'GET', 'it doesn\' look like you\'re an admin!', 'correct message displayed to non-Admin', notAdminCookie);
+                testPayload(server,  '/admindashboard', 'GET', 'it doesn\' look like you\'re an admin!', 'correct message displayed to non-Admin', notAdminCookie);
                 testEndPoint(server, '/admindashboard', 'GET', 302, 'unauth user responds redirected to login');
                 testEndPoint(server, '/adminvacancies', 'GET', 200, 'admin viewing vacancies responds with 200', adminCookie);
                 testEndPoint(server, '/admin/job/test3Vid', 'GET', 200, 'admin gets 200 response', adminCookie);
@@ -42,13 +45,13 @@ server.init(0, (err, server) => {
                 testEndPoint(server, '/rating?agencyEmail=tormodsmith@gmail.com&rating=gold&cvid=' + cvid + '&vid=' + vid, 'GET', 200, 'server responds with redirect', adminCookie);
                 testEndPoint(server, '/admin/job/reject?agencyEmail=tormodsmith@gmail.com&cvid=' + cvid + '&vid=' + vid, 'GET', 200, 'server responds with true', adminCookie);
                 testEndPoint(server, '/admin/job/123', 'GET', 403, 'nonAdmin gets 403 response', notAdminCookie);
-                testEndPoint(server, '/approveuser/approve/VKADAKSD/tormodsmith@gmail.com', 'GET', 200, 'server responds with new view', adminCookie);
+                testEndPoint(server, '/approveuser/approve/iIkUSpzijO/tormodsmith@gmail.com', 'GET', 200, 'server responds with new view', adminCookie);
+                testEndPoint(server, '/approveuser/reject/iIkUSpzijO/tormodsmith@gmail.com', 'GET', 200, 'server responds with new view', adminCookie);
                 testEndPoint(server, '/approveuser/approve/id/me@me.com', 'GET', 302, 'unauthed  redirected to login');
                 testEndPoint(server, '/approveusers', 'GET', 200, 'authed GET responds with 200', adminCookie);
                 testEndPoint(server, '/userinfo/client', 'GET', 200, 'endpoint responds with:', adminCookie);
                 testEndPoint(server, '/userinfo/agencies', 'GET', 200, 'endpoint responds with:', adminCookie);
                 testEndPoint(server, '/admin/job/remove/test3Vid', 'GET', 200, 'server responds with new view', adminCookie);
-
             })
             .then(()=>{
                 client.del('approvedUsers')
--- test/index.js
+++ test/index.js
@@ -2,9 +2,11 @@
 require('./otherplugins.test.js');
 require('./interview.test.js');
 require('./candidateplugins.test.js');
-require('./adminplugins.test.js'); 
+
 require('./harensstalent.test.js');
 */
+
+require('./adminplugins.test.js'); 
 require('./agencyplugins.test.js');
 require('./clientplugins.test.js');
 require('./redis.test.js');
