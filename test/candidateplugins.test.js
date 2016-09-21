63785e103f8fda9ffe9d33151481fa86c8cf41d5
tormod17 <tormodsmith@gmail.com>
2016-09-21 23:41:45 +0100

    email candidate.

--- test/candidateplugins.test.js
+++ test/candidateplugins.test.js
@@ -6,7 +6,6 @@ const candidatePayload = require('./utils/utils.js').candidatePayload;
 
 
 server.init(0, (err, server) => {
-
     testEndPoint(server, '/candidate', 'GET', 200, 'auth user responds with 200', candidateCookie);
     testEndPoint(server, '/candidate', 'POST', 200, 'unauth user redirected to login', candidateCookie, candidatePayload);
     testEndPoint(server, '/candidate', 'POST', 200, 'auth user returns', candidateCookie, candidatePayload);
